6db9c7429fca703eb07a110510bfaf3e6f0d6fc3
bgamwell <bgamwell@gmail.com>
2015-07-24 04:47:50 -0700

    Fixing heroku errors

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,34 +2,34 @@ $(document).ready( function() {
 
   // form error handling using jquery validate
 
-  $('#signup-form').validate({
-    rules: {
-      "user[firstname]": "required",
-      "user[lastname]": "required",
-      "user[email]": "required",
-      "user[password]": {
-        required: true,
-        minlength: 5
-      },
-      verifypassword: {
-        required: true,
-        equalTo: '#password'
-      }
-    },
-
-    messages: {
-      "user[firstname]": "Please enter your first name.",
-      "user[lastname]": "Please enter your last name.",
-      "user[password]": {
-        required: "Please enter a password.",
-        minlength: "Your password must be 5 characters or longer."
-      },
-      verifypassword: {
-        required: "Please verify your password.",
-        equalTo: "Please make sure your passwords match."
-      }
-    }
-  });
+  // $('#signup-form').validate({
+  //   rules: {
+  //     "user[firstname]": "required",
+  //     "user[lastname]": "required",
+  //     "user[email]": "required",
+  //     "user[password]": {
+  //       required: true,
+  //       minlength: 5
+  //     },
+  //     verifypassword: {
+  //       required: true,
+  //       equalTo: '#password'
+  //     }
+  //   },
+  //
+  //   messages: {
+  //     "user[firstname]": "Please enter your first name.",
+  //     "user[lastname]": "Please enter your last name.",
+  //     "user[password]": {
+  //       required: "Please enter a password.",
+  //       minlength: "Your password must be 5 characters or longer."
+  //     },
+  //     verifypassword: {
+  //       required: "Please verify your password.",
+  //       equalTo: "Please make sure your passwords match."
+  //     }
+  //   }
+  // });
 
   //renders ALL logs as a stream to the Dayly homepage
 
